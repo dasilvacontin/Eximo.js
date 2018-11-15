1ac26b3b3ec22ada278c9ed139adfa11b5603eb2
David da Silva <dasilvacontin@gmail.com>
2018-11-15 20:54:45 +0900

    Use XMLHttpRequest instead of jquery

--- Eximo.js
+++ Eximo.js
@@ -1,49 +1,66 @@
-window.jQuery || document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"><\/script>');
-var Eximo = {};
-Eximo.sprites = {};
+let Eximo = {}
+Eximo.sprites = {}
+
 Eximo.getSpriteInfo = function (spriteName) {
-	var info = Eximo.sprites[spriteName];
-	if (!info) console.log('(!) Eximo Error: sprite with name ' + spriteName + ' was not found.');
-	return info;
-};
+  let info = Eximo.sprites[spriteName]
+  if (!info) console.log('(!) Eximo Error: sprite with name ' + spriteName + ' was not found.')
+  return info
+}
+
 Eximo.drawSprite = function (spriteName, context, x, y, w, h) {
-	var spriteInfo = Eximo.getSpriteInfo(spriteName);
-	if (!spriteInfo) return;
-	var frame = spriteInfo.frame;
-	x = x || 0;
-	y = y || 0;
-	w = w || frame.w;
-	h = h || frame.h;
-	context.drawImage(spriteInfo.sheet, frame.x, frame.y, frame.w, frame.h, x, y, w, h);
-};
+  let spriteInfo = Eximo.getSpriteInfo(spriteName)
+  if (!spriteInfo) return
+  let frame = spriteInfo.frame
+  x = x || 0
+  y = y || 0
+  w = w || frame.w
+  h = h || frame.h
+  context.drawImage(spriteInfo.sheet, frame.x, frame.y, frame.w, frame.h, x, y, w, h)
+}
+
 Eximo.getSprite = function (spriteName) {
-	var spriteInfo = Eximo.getSpriteInfo(spriteName);
-	if (!spriteInfo) return;
-	var frame = spriteInfo.frame;
-	var sprite = document.createElement('canvas');
-	sprite.width = frame.w;
-	sprite.height = frame.h;
-	sprite.getContext('2d').drawImage(spriteInfo.sheet, frame.x, frame.y, frame.w, frame.h, 0, 0, frame.w, frame.h);
-	return sprite;
-};
+  let spriteInfo = Eximo.getSpriteInfo(spriteName)
+  if (!spriteInfo) return
+  let frame = spriteInfo.frame
+  let sprite = document.createElement('canvas')
+  sprite.width = frame.w
+  sprite.height = frame.h
+  sprite.getContext('2d').drawImage(spriteInfo.sheet, frame.x, frame.y, frame.w, frame.h, 0, 0, frame.w, frame.h)
+  return sprite
+}
+
 Eximo.loadSheet = function (url, callback) {
-	var sheet = new Image();
-    sheet.onload = function(){callback(sheet);};
-    sheet.src = url;
-};
+  let sheet = new Image()
+  sheet.src = url
+  sheet.onload = function () {
+    callback(sheet)
+  }
+}
+
 Eximo.loadSpriteSheet = function (url, callback) {
-	$.getJSON(url, function(data) {
-		Eximo.loadSheet(data.meta.image, function (img) {
-			for (var spriteName in data.frames) {
-				Eximo.sprites[spriteName] = data.frames[spriteName];
-				Eximo.sprites[spriteName].sheet = img;
-			}
-			callback();
-		});
-	});
-};
+  let req = new XMLHttpRequest()
+  req.addEventListener('load', function () {
+    let data = JSON.parse(req.responseText)
+    Eximo.loadSheet(data.meta.image, function (img) {
+      for (let spriteName in data.frames) {
+        Eximo.sprites[spriteName] = data.frames[spriteName]
+        Eximo.sprites[spriteName].sheet = img
+      }
+      callback()
+    })
+  })
+  req.open('GET', url)
+  req.send()
+}
+
 Eximo.loadSpriteSheets = function (sheetArray, callback) {
-	var sheetsLeft = sheetArray.length;
-	var spriteSheetLoaded = function () {if (--sheetsLeft <= 0) callback();};
-	for (var i = 0; i < sheetArray.length; ++i) Eximo.loadSpriteSheet(sheetArray[i], spriteSheetLoaded);
-};
+  let sheetsLeft = sheetArray.length
+  let spriteSheetLoaded = function () {
+    if (--sheetsLeft <= 0) callback()
+  }
+  for (let i = 0; i < sheetArray.length; ++i) Eximo.loadSpriteSheet(sheetArray[i], spriteSheetLoaded)
+}
+
+if (typeof module === 'object') {
+  module.exports = Eximo
+}
